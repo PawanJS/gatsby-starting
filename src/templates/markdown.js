204429b88ef325f200b8ed818c23b8bd6e2a8763
PawanJS <contactpawanjs@gmail.com>
2021-06-17 07:52:52 +0530

    added traced svg

--- src/templates/markdown.js
+++ src/templates/markdown.js
@@ -33,7 +33,7 @@ export const pageQuery = graphql`
         featuredImage {
           childImageSharp {
             fluid(maxWidth: 800) {
-              ...GatsbyImageSharpFluid
+              ...GatsbyImageSharpFluid_withWebp_tracedSVG
             }
           }
         }
