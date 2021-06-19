1fb61b3cea344eabafc87863a19e59875cd4737e
PawanJS <contactpawanjs@gmail.com>
2021-06-19 08:31:26 +0530

    added plugin manifest and service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,5 +43,24 @@ module.exports = {
     `gatsby-transformer-sharp`, // Needed for dynamic images
     `gatsby-plugin-sass`,
     `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Testing-Gatsby',
+        short_name: 'TG-Blog',
+        start_url: '/',
+        background_color: '#5b3e29',
+        theme_color: '#fdf5e6',
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: 'standalone',
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
+    'gatsby-plugin-offline',
   ],
 };
